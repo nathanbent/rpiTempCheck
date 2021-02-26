a0428542ec1dbaa8eb49beb4ccf1848b0a1505ca
nathanbent <61810169+nathanbent@users.noreply.github.com>
2021-02-25 23:51:46 -0500

    Update bmemain.py

--- bmemain.py
+++ bmemain.py
@@ -1,121 +1,173 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
+# BMEmain Script
+# V .5 - MySQL, April 2020
+# V 1.0 - InfluxDB, May 2020
+# V 1.5 - Modularity, to be called in other programs.  Feb 2021
+
+# Want to do
+# Improve readability, improve robustness
 import time
 import board
 import busio
 import adafruit_bme280
 from influxdb import InfluxDBClient
-#import all your stuff 
- 
-# Create library object using our Bus I2C port
+from datetime import datetime
+
+time_between_reads = .5
+time_between_reads_0 = 0  # For when this file gets imported
+
+credentials = ['host_name', 'influxdb_host', 'influxdb_port', 'influxdb_user', 'influxdb_pass', 'influxdb_db']
+built_in_credentials = False
+credentials_file = "credentials.txt"
 i2c = busio.I2C(board.SCL, board.SDA)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
- 
-# OR create library object using our Bus SPI port
-# spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-# bme_cs = digitalio.DigitalInOut(board.D10)
-# bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
- 
-# change this to match the location's pressure (hPa) at sea level
-bme280.sea_level_pressure = 1013.25
-min_between_reads = .5
-#Minutes beteween reads for while loop
-#This should help to set basic parameters for what to expect to help weed out bad results
-oldtemp = 75
-oldpressure = 1005
-oldhumidity = 50
-errorscorrected = 0
-runcount = 0
 
 
-while True:
- 
-    def tempRead(): #read temperature, return float with 3 decimal places  
-            fakedegrees = bme280.temperature  
-            bmedegrees = (fakedegrees * 1.8) + 32
-            return bmedegrees
-      
-    def pressRead():#read pressure, return float with 3 decimal places  
-            bmepascals = bme280.pressure
-            return bmepascals  
-      
-    def humidityRead(): #read humidity, return float with 3 decimal places  
-            bmehumidity = bme280.humidity 
-            return bmehumidity
-    
-    temptemp = tempRead()  
-    temppressure = pressRead()  
-    temphumidity = humidityRead()
+def temp_read():  # read temperature, return float with 3 decimal places
+    fake_degrees = bme280.temperature
+    bme_degrees = (fake_degrees * 1.8) + 32
+    return bme_degrees
 
-    
-#This should weed out bad results    
 
-#This will make sure that it is run once to establish a baseline, and then scale from there
-    if runcount > 0:
+def humidity_read():  # read humidity, return float with 3 decimal places
+    bme_humidity = bme280.humidity
+    return bme_humidity
 
-        if abs(temptemp - oldtemp) > 10:
-            temperature = oldtemp
-            errorscorrected = errorscorrected + 1 #count errors
-        else:
-            temperature = temptemp
-            oldtemp = temptemp #allow for results to scale
-            
-        if abs(temppressure - oldpressure) > 10:
-            pressure = oldpressure
-            errorscorrected = errorscorrected + 1
-        else:
-            pressure = temppressure
-            oldpressure = temppressure
-        
-        if abs(temphumidity - oldhumidity) > 5:
-            humidity = oldhumidity
-            errorscorrected = errorscorrected + 1
-        else:
-            humidity = temphumidity
-            oldhumidity = humidity
-    else:
-        temperature = temptemp
-        oldtemp = temptemp
-        pressure = temppressure
-        oldpressure = temppressure
-        humidity = temphumidity
-        oldhumidity = temphumidity
 
-#this section will count how many times the script has run and will then calculate the amount of seconds it has been running
+def press_read():  # read pressure, return float with 3 decimal places
+    bme_pascals = bme280.pressure
+    return bme_pascals
 
-    runcount = runcount + 1
-    runtime = runcount * (60*min_between_reads) - 30
 
+def credentials_setup():
+    global credentials
+    global credentials_file
+    try:
+        with open(credentials_file) as f:
+            credentials = f.read().splitlines()
+    except OSError:
+        credentials_file_open = open(credentials_file, 'w')
+        spot = 0
+        for value in credentials:
+            input_value = input("What is the " + value + " for this program? ")
+            credentials[spot] = input_value
+            credentials_file_open.write(input_value + '\n')
+            spot += 1
+
+
+def write_to_influx(temperature, humidity, pressure, errors_corrected, run_count, run_time, influx_credentials):
+    global credentials
+    host_name, influxdb_host, influxdb_port, influxdb_user, influxdb_pass, influxdb_db = influx_credentials
     bme280_data = [
         {
-            "measurement" : "bme280",
-            "tags" : {
-                "host": "RaspiTest"
+            "measurement": "bme280",
+            "tags": {
+                "host": host_name
             },
-            "fields" : {
+            "fields": {
                 "temperature": float(temperature),
                 "humidity": float(humidity),
                 "pressure": float(pressure),
-                "errorscorrected" : float(errorscorrected),
-                "readruncount" : float(runcount),
-                "readruntime" : float(runtime)
+                "errorscorrected": float(errors_corrected),
+                "readruncount": float(run_count),
+                "readruntime": float(run_time)
             }
         }
     ]
 
+    try:
+        client = InfluxDBClient(influxdb_host, influxdb_port, influxdb_user, influxdb_pass, influxdb_db)
+
+        client.write_points(bme280_data)
+    except:
+        print("Error encountered BME, waiting for next pass to try again")
 
 
-    client = InfluxDBClient('192.168.1.15', 8086, 'bme280monitor', 'Subaru15', 'bme280')
+def bme280_check_script(credentials):
+    global credentials_file
+    global time_between_reads
+    bme280.sea_level_pressure = 1013.25
+    # This should help to set basic parameters for what to expect to help weed out bad results
+    old_temp = 75
+    old_humidity = 50
+    old_pressure = 1005
+    errors_corrected = 0
+    run_count = 0
+    start_time = time.time()
+    run_loop = True
+    while run_loop is True:
 
-    client.write_points(bme280_data)
+        temp_temp = temp_read()
+        temp_humidity = humidity_read()
+        temp_pressure = press_read()
+
+        # This should weed out bad results
+        # This will make sure that it is run once to establish a baseline, and then scale from there
+        if run_count > 0:
+
+            if abs(temp_temp - old_temp) > 5 * time_between_reads:
+                temperature = old_temp
+                errors_corrected = errors_corrected + 1  # count errors
+            else:
+                temperature = temp_temp
+                old_temp = temp_temp  # allow for results to scale
+            if abs(temp_humidity - old_humidity) > 25 * time_between_reads:
+                humidity = old_humidity
+                errors_corrected = errors_corrected + 1
+            else:
+                humidity = temp_humidity
+                old_humidity = humidity
+            if abs(temp_pressure - old_pressure) > 10 * time_between_reads:
+                pressure = old_pressure
+                errors_corrected = errors_corrected + 1
+            else:
+                pressure = temp_pressure
+                old_pressure = temp_pressure
+
+        else:
+            temperature = temp_temp
+            old_temp = temp_temp
+            humidity = temp_humidity
+            old_humidity = temp_humidity
+            pressure = temp_pressure
+            old_pressure = temp_pressure
 
 
-    
-    
-    print("\nTemperature: %0.1f F" % temperature)
-    print("Humidity: %0.1f %%" % humidity)
-    print("Pressure: %0.1f hPa" % pressure)
-    print("Run count: %0.1f" % runcount)
-    print("Run time: %0.1f" % runtime)
-    print("Errors Corrected: %0.1f" % errorscorrected)
-    #print("Altitude = %0.2f meters" % bme280.altitude)
-    time.sleep(60*min_between_reads)
+        # this section will count how many times the script has run
+        # and will then calculate the amount of seconds it has been running
+
+        run_count = run_count + 1
+        run_time = time.time() - start_time
+
+        write_to_influx(temperature, humidity, pressure, run_count, run_time, errors_corrected, credentials)
+
+
+
+        print("\nTemperature: %0.1f F" % temperature)
+        print("Humidity: %0.1f %%" % humidity)
+        print("Pressure: %0.1f hPa" % pressure)
+        print("Run count: %0.1f" % run_count)
+        print("Run time: %0.1f" % run_time)
+        print("Errors Corrected: %0.1f" % errors_corrected)
+        # print("Altitude = %0.2f meters" % bme280.altitude)
+        if __name__ == "__main__":
+            time.sleep(60 * time_between_reads)
+        else:
+            run_loop = False
+            return temperature, humidity
+
+def bme_main():
+    global credentials
+    if built_in_credentials is True:
+        credentials = credentials
+    else:
+        credentials_setup()
+    bme280_check_script(credentials)
+
+
+def main():
+    bme_main()
+
+
+if __name__ == "__main__":
+    main()
